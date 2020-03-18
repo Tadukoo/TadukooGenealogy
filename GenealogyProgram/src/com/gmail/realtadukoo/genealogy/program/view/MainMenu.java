b17dbe65c178b0fb21c6d37152411ce733a8af6a
Tadukoo <realtadukoo@gmail.com>
2020-03-17 20:31:02 -0400

    Minor Changes working with Tadukoo Util/Engine progress
    - Mainly committing due to transitioning to a new laptop
    - Also includes updated pom to make a proper executable file

--- GenealogyProgram/src/com/gmail/realtadukoo/genealogy/program/view/MainMenu.java
+++ GenealogyProgram/src/com/gmail/realtadukoo/genealogy/program/view/MainMenu.java
@@ -1,23 +1,33 @@
 package com.gmail.realtadukoo.genealogy.program.view;
 
-import java.awt.Color;
-
 import com.gmail.realtadukoo.util.event.view.ViewChangeEvent;
 import com.gmail.realtadukoo.util.view.Context;
 import com.gmail.realtadukoo.util.view.View;
-import com.gmail.realtadukoo.util.view.draw.Text;
+import com.gmail.realtadukoo.util.view.draw.Draw.ORIENTATION;
+import com.gmail.realtadukoo.util.view.textinput.TextInputPrompt;
 
 public class MainMenu extends View{
 	
 	@Override
 	public void init(Context context){
 		super.init(context);
+		addClickable(0, TextInputPrompt.builder()
+										.x(1366/2-100)
+										.y(768/2-25)
+										.width(200)
+										.height(50)
+										.textOrientation(ORIENTATION.CENTER)
+										.textX(100)
+										.textY(25)
+										.build());
+		/*
 		addDrawable(0, Text.builder()
 							.text("Derp")
 							.x(1366/2)
 							.y(768/2)
 							.color(Color.RED)
 							.build());
+		/**/
 	}
 	
 	@Override
